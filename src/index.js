d3adc7757f730475cf74e34831ef7e8b2f5aae47
Vamsi <s.vamsirao@gmail.com>
2017-10-09 21:15:21 +0530

    Add untracked files

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import promise from 'redux-promise';
 import reducers from './reducers';
 import PostsIndex from './components/PostsIndex';
 import PostsNew from './components/PostsNew';
+import PostsShow from './components/PostsShow';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -16,6 +17,8 @@ ReactDOM.render(
   	<BrowserRouter>
   		<Switch>
 		  	<Route path="/posts/new" component={PostsNew} />
+			{/* We didn't put :id before becuase it is a wildcard token and /new could've been mistakenly taken as the token */}
+		  	<Route path="/posts/:id" component={PostsShow} />
   			<Route path="/" component={PostsIndex} />
   		</Switch>
   	</BrowserRouter>
